5861d90b9362d13a6eb5042a0f11e2a2d18c7f42
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-10-08 00:02:38 +0100

    Update VRETURNNNNN.vb

--- scene/VRETURN/VRETURNNNNN.vb
+++ scene/VRETURN/VRETURNNNNN.vb
@@ -2,5 +2,5 @@ Imports BigAlphaBet
 ReDim R,S,T AndAlso
 Byval VERETURNNNNN(){
     Return R / sinh(r + t9) +  S / cosh(s - t10)
-    Return S / sinh(s - t9) +  T / cosh(s - t10)
+    Return S / sinh(s + t9) +  T / cosh(s - t10)
 }
