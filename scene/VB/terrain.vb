b77df90546a83460ddd121b9f335d0a1562ef77f
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-10-09 13:40:23 +0800

    Update terrain.vb

--- scene/VB/terrain.vb
+++ scene/VB/terrain.vb
@@ -1,3 +1,6 @@
+Imports "15.vb"
+imports "color.css"
+Imports "terrain.vb"
 Imports System.Drawing
 Module Module1
     ' 地形生成函数
@@ -7,57 +10,48 @@ Module Module1
             For x As Integer = 0 To width + 1
                 For y As Integer = 0 To height + 1
                     For z As Integer = 0 To depth + 1
-                    ' 使用Perlin噪声生成高度值
+                    ' 使用Perlin噪声生成高度值    ' 将噪声值映射到颜色深度 ' 设置像素颜色
                     Dim noiseValue As Double = PerlinNoise(x, y, z)
-                    ' 将噪声值映射到颜色深度
                     Dim color As Color = MapToColor(noiseValue)
-                    ' 设置像素颜色
                     graphics.FillRectangle(New SolidBrush(color), x, y, z, 1, 1)
                 Next
             Next
         End Using
         Return bitmap
     End Function
-    ' Perlin噪声函数
+    ' Perlin噪声函数 ' 这里应该是Perlin噪声的具体实现代码' 为了示例简便，这里返回一个随机值 ' 将噪声值映射到颜色 ' 根据噪声值调整颜色深浅
     Function PerlinNoise(x As Integer, y As Integer, z As Integer) As Double
-        ' 这里应该是Perlin噪声的具体实现代码
-        ' 为了示例简便，这里返回一个随机值
         Return Rnd()
     End Function
-
-    ' 将噪声值映射到颜色
     Function MapToColor(value As Double) As Color
-        ' 根据噪声值调整颜色深浅
         Dim hue As Single = value * 0.5
         Dim saturation As Single = 1.0
         Dim brightness As Single = 0.5
         Return Color.FromArgb(255, HueToRGB(hue), SaturationToRGB(saturation), BrightnessToRGB(brightness))
     End Function
-    ' HSL颜色转换为RGB
+    ' HSL颜色转换为RGB Color translation I     
+    ' 这里应该是HSL转RGB的具体实现代码 Concrect Published Code
+    ' 为了示例简便，这里返回一个固定值 in order to give a example in convince,here return a specific value
+    ' HSL颜色转换为RGB Color translation II
+    ' 这里应该是HSL转RGB的具体实现代码
+    ' 为了示例简便，这里返回一个固定值
+    ' HSL颜色转换为RGB Color translation III
+    ' 这里应该是HSL转RGB的具体实现代码
+    ' 为了示例简便，这里返回一个固定值
+    ' 生成一个16*16*16的无界地形 no boundary terrain
+    ' 保存或显示地形图像
+    ' terrain.Show()
     Function HueToRGB(hue As Single) As Byte
-        ' 这里应该是HSL转RGB的具体实现代码
-        ' 为了示例简便，这里返回一个固定值
-        Return 255
+        Return 255,0,0
     End Function
-    ' HSL颜色转换为RGB
     Function SaturationToRGB(saturation As Single) As Byte
-        ' 这里应该是HSL转RGB的具体实现代码
-        ' 为了示例简便，这里返回一个固定值
-        Return 255
+        Return 0,255,0
     End Function
-
-    ' HSL颜色转换为RGB
     Function BrightnessToRGB(brightness As Single) As Byte
-        ' 这里应该是HSL转RGB的具体实现代码
-        ' 为了示例简便，这里返回一个固定值
-        Return 255
+        Return 0,0,255
     End Function
     Sub Main()
-        ' 生成一个100x100的无界地形
         Dim terrain As Bitmap = GenerateNoBoundaryTerrain(16, 16, 16)
-        ' 保存或显示地形图像
         terrain.Save()
-        ' terrain.Show()
     End Sub
-
 End Module
