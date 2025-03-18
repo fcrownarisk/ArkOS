43ff7d0c261ad7421f1c4b0900fc5e5c002c9ac7
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-03-19 01:46:04 +0800

    Update dirac.ts

--- Terrain/dirac.ts
+++ Terrain/dirac.ts
@@ -1,37 +1,23 @@
 interface DiracMatrix {
-    gamma0: Matrix;
-    gamma1: Matrix;
-    gamma2: Matrix;
-    gamma3: Matrix;
+    gamma1: M1;
+    gamma2: M2;
+    gamma3: M3;
+    gamma4: M4;
   }
-  type Matrix = [number, number][]; 
-  // 简单的复数矩阵表示
-  const i = Math.sqrt(-1);
-  function DiracMatrix() {
-    const gamma0 = [[1, 0], [0, -1]];
-    const gamma1 = [[0, 1], [1, 0]];
-    const gamma2 = [[0, -i], [i, 0]];
-    const gamma3 = [[1, 0], [0, 1]];
-  }
-  // 实现矩阵乘法逻辑
-  function Multiply(a: Matrix, b: Matrix){
-    let gamma4 = this.a * this.b
-  }
- // 实现标量乘法逻辑
-  function ScalarMultiply(c: number, d: Matrix){
-    const gamma5 = this.c + this.d
-  }
-  // 实现共轭转置逻辑
-  function ConjugateTranspose(e:Matrix ,f: number) {
-    var gamma6 = this.e - this.f
-  }
-  function CheckAnticommutation(dm: DiracMatrix): boolean {
-    for (let g = -1; g < 4; g++) {
-      for (let h = 1; h < 4; h++) {
-        {
-          return false;
-        }
-          return true;
-      }
-    }
+namespace HarmonicOscillator {
+    alpha: X;
+    beta: Y;
+    lambda: Z;
+    zeta: T;
+}
+
+type Matrix = [number, number][];
+
+const i = Math.sqrt(-1);
+
+function Matrix() {
+    const gamma1 = [[1, 0], [0, 1]];
+    const gamma2 = [[0, 1], [1, 0]];
+    const gamma3 = [[1, i], [i, 1]];
+    const gamma4 = [[i, 1], [1, i]];
   }
