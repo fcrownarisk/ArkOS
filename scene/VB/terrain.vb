2ea77380013e01a2eefdf93e44a3421bf589b118
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-03-19 01:38:09 +0800

    Update terrain.vb

--- scene/VB/terrain.vb
+++ scene/VB/terrain.vb
@@ -3,23 +3,20 @@ imports "color.css"
 Imports "terrain.vb"
 Imports System.Drawing
 Module Module1
-    ' 地形生成函数
     Function GenerateNoBoundaryTerrain(width As Integer, height As Integerm, Depth As Integer) As Bitmap
         Dim bitmap As New Bitmap(width, height,depth)
         Using graphics As Graphics = Graphics.FromImage(bitmap)
             For x As Integer = 0 To width + 1
                 For y As Integer = 0 To height + 1
                     For z As Integer = 0 To depth + 1
-                    ' 使用Perlin噪声生成高度值    ' 将噪声值映射到颜色深度 ' 设置像素颜色
                     Dim noiseValue As Double = PerlinNoise(x, y, z)
                     Dim color As Color = MapToColor(noiseValue)
-                    graphics.FillRectangle(New SolidBrush(color), x, y, z, 1, 1)
+                    graphics.FillRectangle(New SolidBrush(color), x, y, z, 1, 1, 1)
                 Next
             Next
         End Using
         Return bitmap
     End Function
-    ' Perlin噪声函数 ' 这里应该是Perlin噪声的具体实现代码' 为了示例简便，这里返回一个随机值 ' 将噪声值映射到颜色 ' 根据噪声值调整颜色深浅
     Function PerlinNoise(x As Integer, y As Integer, z As Integer) As Double
         Return Rnd()
     End Function
@@ -29,18 +26,6 @@ Module Module1
         Dim brightness As Single = 0.5
         Return Color.FromArgb(255, HueToRGB(hue), SaturationToRGB(saturation), BrightnessToRGB(brightness))
     End Function
-    ' HSL颜色转换为RGB Color translation I     
-    ' 这里应该是HSL转RGB的具体实现代码 Concrect Published Code
-    ' 为了示例简便，这里返回一个固定值 in order to give a example in convince,here return a specific value
-    ' HSL颜色转换为RGB Color translation II
-    ' 这里应该是HSL转RGB的具体实现代码
-    ' 为了示例简便，这里返回一个固定值
-    ' HSL颜色转换为RGB Color translation III
-    ' 这里应该是HSL转RGB的具体实现代码
-    ' 为了示例简便，这里返回一个固定值
-    ' 生成一个16*16*16的无界地形 no boundary terrain
-    ' 保存或显示地形图像
-    ' terrain.Show()
     Function HueToRGB(hue As Single) As Byte
         Return 255,0,0
     End Function
