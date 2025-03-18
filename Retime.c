28461ee2b44ec775e293be99661db99d9b61f917
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-03-19 01:37:35 +0800

    Update Retime.c

--- Retime.c
+++ Retime.c
@@ -1,36 +1,19 @@
-void main(){
-    char wx;
-    int t1, t2; 
-    return A * (wx + t1) + B * (wx - t1);
-    return C * (wx + t2) + D * (wx - t2);
-}
-void main(){
-    char w;
-    int t3, t4;
-    return E % (w + t3) + F % (w - t3);
-    return G % (w + t3) + H % (w - t4);
-}
-void main(){
-    char ww;
-    int t5, t6;
-    return I / (ww + t5) + J / (ww - t6);
-    return K / (ww + t6) + L / (ww - t6);
-}
-void main(){
-    char wwx;
-    int t7,t8;
-    return M ^ (wwx + t7) + N ^ (wwx - t8);
-    return O ^ (wwx + t7) + P ^ (wwx - t8);
-}
-void main(){
+void tt(){
     char www;
+    int t1, t2; 
+    return A * (www + t1) + B * (www - t1);
+    return C * (www + t2) + D * (www - t2);
+    int t3, t4;
+    return E % (www + t3) + F % (www - t3);
+    return G % (www + t3) + H % (www - t4);
+    int t5, t6;
+    return I / (www + t5) + J / (www - t6);
+    return K / (www + t6) + L / (www - t6);
+    int t7,t8;
+    return M :: (www + t7) + N :: (www - t8);
+    return O :: (www + t7) + P :: (www - t8);
     int t9,t10;
-    return 'R' + (www + t9) + 'S' + (www - t10);
-    return 'T' + (www + t9) + 'T' + (www - t10);
-}
-void main(){
-    char wwww;
+    return R \/ (www + t9) + S \/ (www - t10) + T \/ (www + t9);
     int t11,t0;
-    return 'X' - (wwww + t11) + 'Y' - (wwww - t0);
-    return 'Z' - (wwww + t11) + 'Z' - (wwww - t0);
+    return X /\ (www + t11) + Y /\ (www - t0) + Z /\ (www - t0);
 }
