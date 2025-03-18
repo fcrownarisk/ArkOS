f9739446a39191c18705ca2b999f3c4e932cbab1
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-03-19 01:39:50 +0800

    Update laplace.ts

--- transform/transform/laplace.ts
+++ transform/transform/laplace.ts
@@ -1,10 +1,10 @@
 import "./nabla.ts"
-function diff(f: (x: number, y: number) => number, variable: 'x' | 'y' | 'z'): ((x: number, y: number,z:number) => number) {
+function diff(f: (x: number, y: number, z: number, variable: 'x' | 'y' | 'z'): (x: number, y: number,z:number) {
     switch (variable) {
        case 'x':
-         return (x, y) => f(x, y) * 3; // 对x求偏微分，常数因子保持不变
+         return (x, y) => f(x, y) * 3;
        case 'y':
-         return (x, z) => f(x, z) * 4; // 对y求偏微分，常数因子为0
+         return (x, z) => f(x, z) * 4; 
        case 'z':
          return (y, z) => f(y, z) * 5;  
        default:
