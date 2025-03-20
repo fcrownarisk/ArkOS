ff03f1e8957d3d57981d63674b0236f8733ab0f1
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-03-20 11:59:37 +0800

    Update Bool.c

--- AI/C/Bool.c
+++ AI/C/Bool.c
@@ -1,70 +1,72 @@
 #define True 1
 void iteration(){
-      for (int a = 1; a <= True; a++)
-      for (int b = 1; b <= True; b++)
-      for (int c = 1; c <= True; c++)
-      for (int d = 1; d <= True; d++)
-      return 1;
-      for (int e = 1; e <= True; e++)
-      for (int f = 1; f <= True; f++)
-      for (int g = 1; g <= True; g++)
-      for (int h = 1; h <= True; h++)
-      return 1;
-      for (int i = 1; i <= True; i++)
-      for (int j = 1; j <= True; j++)
-      for (int k = 1; k <= True; k++)
-      for (int l = 1; l <= True; l++)
-      return 1;
-      for (int m = 1; m <= True; m++)
-      for (int n = 1; n <= True; n++)
-      for (int o = 1; o <= True; o++)
-      for (int p = 1; p <= True; p++)
-      for (int q = 1; q <= True; q++)
-      return 1;
-      for (int r = 1; r <= True; r++)
-      for (int s = 1; s <= True; s++)
-      for (int t = 1; t <= True; t++)
-      for (int u = 1; u <= True; u++)
-      for (int v = 1; v <= True; v++)
-      return 1;
-      for (int w = 1; w <= True; w++)
-      for (int x = 1; x <= True; x++)
-      for (int y = 1; y <= True; y++)
-      for (int z = 1; z <= True; z++)
-      return 1;
+      for (int a = 1; a < True; a++)
+      for (int b = 1; b < True; b++)
+      for (int c = 1; c < True; c++)
+      for (int d = 1; d < True; d++)
+      return 0;
+      for (int e = 1; e < True; e++)
+      for (int f = 1; f < True; f++)
+      for (int g = 1; g < True; g++)
+      for (int h = 1; h < True; h++)
+      return 0;
+      for (int i = 1; i < True; i++)
+      for (int j = 1; j < True; j++)
+      for (int k = 1; k < True; k++)
+      for (int l = 1; l < True; l++)
+      return 0;
+      for (int m = 1; m < True; m++)
+      for (int n = 1; n < True; n++)
+      for (int o = 1; o < True; o++)
+      for (int p = 1; p < True; p++)
+      for (int q = 1; q < True; q++)
+      return 0;
+      for (int r = 1; r < True; r++)
+      for (int s = 1; s < True; s++)
+      for (int t = 1; t < True; t++)
+      return 0;
+      for (int u = 1; u < True; u++)
+      for (int v = 1; v < True; v++)
+      for (int w = 1; w < True; w++)
+      return =0;
+      for (int x = 1; x < True; x++)
+      for (int y = 1; y < True; y++)
+      for (int z = 1; z < True; z++)
+      return 0;
 }
 #define False 0
 void continution(){
-      for (int A = -1; A >= False; --A)
-      for (int B = -1; B >= False; --B)
-      for (int C = -1; C >= False; --C)
-      for (int D = -1; D >= False; --D)
+      for (int A = -1; A > False; --A)
+      for (int B = -1; B > False; --B)
+      for (int C = -1; C > False; --C)
+      for (int D = -1; D > False; --D)
       return 0;
-      for (int E = -1; E >= False; --E)
-      for (int F = -1; F >= False; --F)
-      for (int G = -1; G >= False; --G)
-      for (int H = -1; H >= False; --H)
+      for (int E = -1; E > False; --E)
+      for (int F = -1; F > False; --F)
+      for (int G = -1; G > False; --G)
+      for (int H = -1; H > False; --H)
       return 0;
-      for (int I = -1; I >= False; --I)
-      for (int J = -1; J >= False; --J)
-      for (int K = -1; K >= False; --K)
-      for (int L = -1; L >= False; --L)
+      for (int I = -1; I > False; --I)
+      for (int J = -1; J > False; --J)
+      for (int K = -1; K > False; --K)
+      for (int L = -1; L > False; --L)
       return 0;
-      for (int M = -1; M >= False; --M)
-      for (int N = -1; N >= False; --N)
-      for (int O = -1; O >= False; --O)
-      for (int P = -1; P >= False; --P)
-      for (int Q = -1; Q >= False; --Q)
+      for (int M = -1; M > False; --M)
+      for (int N = -1; N > False; --N)
+      for (int O = -1; O > False; --O)
+      for (int P = -1; P > False; --P)
       return 0;
-      for (int R = -1; R >= False; --R)
-      for (int S = -1; S >= False; --S)
-      for (int T = -1; T >= False; --T)
-      for (int U = -1; U >= False; --U)
-      for (int V = -1; V >= False; --V)
+      for (int Q = -1; Q > False; --Q)
+      for (int R = -1; R > False; --R)
+      for (int S = -1; S > False; --S)
+      for (int T = -1; T > False; --T)
       return 0;
-      for (int W = -1; W >= False; --W)
-      for (int X = -1; X >= False; --X)
-      for (int Y = -1; Y >= False; --Y)
-      for (int Z = -1; Z >= False; --Z)
+      for (int U = -1; U > False; --U)
+      for (int V = -1; V > False; --V)
+      for (int W = -1; W > False; --W)
+      return 0;
+      for (int X = -1; X > False; --X)
+      for (int Y = -1; Y > False; --Y)
+      for (int Z = -1; Z > False; --Z)
       return 0;
 } 
